23d63ac415a20cdd2d233bfd6b6642b1703ac29f
Srishti R Deo <srishti.deo@beautosys.com>
2019-12-06 11:44:31 +0530

    changes for added validation massage made mohini

--- src/app/components/forgot/forgot.component.ts
+++ src/app/components/forgot/forgot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.component.scss']
 })
 export class ForgotComponent implements OnInit {
+  
+  forgetForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.forgetForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
-
+  get f() { return this.forgetForm.controls; }
+  onSubmit(){
+    this.submitted=true;
+    if(this.forgetForm.invalid){
+      return;
+  
+    }
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+     phoneno: ['', [Validators.required ,Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+    password: ['', [Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
+    captcha: ['',Validators.required],
+    rememberset:['']
+  });
   }
+  get f() { return this.loginForm.controls; }
 
+  
+  onSubmit(){
+  this.submitted=true;
+  if(this.loginForm.invalid){
+    return;
+
+  }
+}
 }
