3e2391633e359eae888931dc46855b917163f5c9
dhanshriautade <dhanshri.autade2#gmail.com>
2020-01-04 10:12:34 +0530

    changes

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -5,15 +5,18 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-selector: 'app-admin',
-templateUrl: './admin.component.html',
-styleUrls: ['./admin.component.scss']
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-display = false;
-employeeForm = new FormGroup({
+  display = false;
+  employeeForm = new FormGroup({
+  });
 
-  })
+  departmentsAndDesignations: any = [];
+
+  designationList: any = [];
   devicedetails
   displaylist = true;
   useradd: boolean = false;
@@ -23,53 +26,69 @@ employeeForm = new FormGroup({
   documentArray = [];
   docidArray = [];
   term;
-  spinner=false;
+  spinner = false;
   dataone;
   separateDialCode = true;
   SearchCountryField = SearchCountryField;
   TooltipLabel = TooltipLabel;
   CountryISO = CountryISO;
   infodetail = [];
-  configer:any;
+  configer: any;
   preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
-  constructor(private formBuilder: FormBuilder, private toastr: ToastrService, public EmployeeService: EmployeeService) { 
-    
-    this.EmployeeService.getEmployee().subscribe(res => {
-       this.data = res;
-        let keys = Object.keys(this.data);
-        console.log('keys',keys);
-        for(var i=0; i<keys.length ; i++){
-           this.infodetail.push(this.data[keys[i]]);
-        }
-        this.configer = {
-          itemsPerPage: 8,
-          currentPage: 1,
-          totalItems: this.infodetail.length
-        };
-       console.log('list',this.infodetail);
-    })
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService, public EmployeeService: EmployeeService) {
 
-}
-pageChanged(event){
-  this.configer.currentPage = event;
-}
-showDialog() {
-this.display = true;
-}
-AddDevice() {
-this.useradd = true;
-}
+    this.departmentsAndDesignations = [['Software', ["Android", "IOS", "Java"]], ["Embedded", ["Embedded department 1", "Embedded department 2", "Embedded department 3"]], ["Mechanical", ["Mechanical department 1", "Mechanical department 2", "Mechanical department 3"]]];
+    this.getAllEmployeesList();
+  }
+
+  getDesignationList() {
+    let dept = this.employeeForm.get('department').value;
+    let map = new Map(this.departmentsAndDesignations);
+
+    for (let entry of map.entries()) {
+      if (dept === entry[0]) {
+        this.designationList = entry[1];
+      }
+    }
+  }
+
+  getAllEmployeesList() {
+    this.EmployeeService.getEmployee().subscribe(res => {
+      this.infodetail = [];
+      this.data = res;
+      let keys = Object.keys(this.data);
+      console.log('keys', keys);
+      for (var i = 0; i < keys.length; i++) {
+        this.infodetail.push(this.data[keys[i]]);
+      }
+      this.configer = {
+        itemsPerPage: 8,
+        currentPage: 1,
+        totalItems: this.infodetail.length
+      };
+      console.log('list', this.infodetail);
+    });
+  }
+  pageChanged(event) {
+    this.configer.currentPage = event;
+  }
+  showDialog() {
+    this.display = true;
+  }
+  AddDevice() {
+    this.useradd = true;
+  }
 
 
 
   ngOnInit() {
     this.employeeForm = this.formBuilder.group({
-      title: ['',Validators.required],
+      title: ['', Validators.required],
       firstName: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
       lastName: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
       email: ['', [Validators.required, Validators.email]],
       //phoneNo: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
-      phone:['', Validators.required],
+      phone: ['', Validators.required],
       dob: ['', Validators.required],
       ID: ['', Validators.required],
       idno: ['', Validators.required],
@@ -79,96 +98,117 @@ this.useradd = true;
       drivingLicence: [''],
       voterId: ['',],
       status: [''],
-    
+      department: [''],
+      designation: ['']
     })
   }
 
-get f() { return this.employeeForm.controls; }
-onSubmit() {
-this.submitted = true;
-this.spinner = true;
-if (this.employeeForm.invalid) {
-return;
-}
-if (this.employeeForm.value.ID == 'Passport') {
-this.employeeForm.value.passport = this.employeeForm.value.idno;
-this.employeeForm.value.pan = '';
-this.employeeForm.value.adhar = '';
-this.employeeForm.value.drivingLicence = '';
-this.employeeForm.value.voterId = ''
-} else if (this.employeeForm.value.ID == 'PAN Card') {
-this.employeeForm.value.passport = '';
-this.employeeForm.value.pan = this.employeeForm.value.idno;
-this.employeeForm.value.adhar = '';
-this.employeeForm.value.drivingLicence = '';
-this.employeeForm.value.voterId = ''
-} else if (this.employeeForm.value.ID == 'Adhar Card') {
-this.employeeForm.value.passport = '';
-this.employeeForm.value.pan = ''
-this.employeeForm.value.adhar = this.employeeForm.value.idno;
-this.employeeForm.value.drivingLicence = '';
-this.employeeForm.value.voterId = ''
-} else if (this.employeeForm.value.ID == 'Driving Lincese') {
-this.employeeForm.value.passport = '';
-this.employeeForm.value.pan = '';
-this.employeeForm.value.adhar = '';
-this.employeeForm.value.drivingLicence = this.employeeForm.value.idno;
-this.employeeForm.value.voterId = ''
-} else if (this.employeeForm.value.ID == 'Voter ID') {
-this.employeeForm.value.passport = '';
-this.employeeForm.value.pan = '';
-this.employeeForm.value.adhar = '';
-this.employeeForm.value.drivingLicence = '';
-this.employeeForm.value.voterId = this.employeeForm.value.idno;
-}
+  get f() { return this.employeeForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    this.spinner = true;
+    if (this.employeeForm.invalid) {
+      return;
+    }
+    // if (this.employeeForm.value.ID == 'Passport') {
+    //   this.employeeForm.value.passport = this.employeeForm.value.idno;
+    //   this.employeeForm.value.pan = '';
+    //   this.employeeForm.value.adhar = '';
+    //   this.employeeForm.value.drivingLicence = '';
+    //   this.employeeForm.value.voterId = ''
+    // } else if (this.employeeForm.value.ID == 'PAN Card') {
+    //   this.employeeForm.value.passport = '';
+    //   this.employeeForm.value.pan = this.employeeForm.value.idno;
+    //   this.employeeForm.value.adhar = '';
+    //   this.employeeForm.value.drivingLicence = '';
+    //   this.employeeForm.value.voterId = ''
+    // } else if (this.employeeForm.value.ID == 'Adhar Card') {
+    //   this.employeeForm.value.passport = '';
+    //   this.employeeForm.value.pan = ''
+    //   this.employeeForm.value.adhar = this.employeeForm.value.idno;
+    //   this.employeeForm.value.drivingLicence = '';
+    //   this.employeeForm.value.voterId = ''
+    // } else if (this.employeeForm.value.ID == 'Driving Lincese') {
+    //   this.employeeForm.value.passport = '';
+    //   this.employeeForm.value.pan = '';
+    //   this.employeeForm.value.adhar = '';
+    //   this.employeeForm.value.drivingLicence = this.employeeForm.value.idno;
+    //   this.employeeForm.value.voterId = ''
+    // } else if (this.employeeForm.value.ID == 'Voter ID') {
+    //   this.employeeForm.value.passport = '';
+    //   this.employeeForm.value.pan = '';
+    //   this.employeeForm.value.adhar = '';
+    //   this.employeeForm.value.drivingLicence = '';
+    //   this.employeeForm.value.voterId = this.employeeForm.value.idno;
+    // }
 
 
-this.data = {
-"title": this.employeeForm.value.title,
-"firstName": this.employeeForm.value.firstName,
-"lastName": this.employeeForm.value.lastName,
-"email": this.employeeForm.value.email,
-//"phoneNo": this.employeeForm.value.phoneNo,
-"contact": this.employeeForm.value.phone.number,
-"dob": this.employeeForm.value.dob,
-"passport": this.employeeForm.value.passport,
-"pan": this.employeeForm.value.pan,
-"adhar": this.employeeForm.value.adhar,
-"drivingLicence": this.employeeForm.value.drivingLicence,
-"voterId": this.employeeForm.value.voterId,
-"status": "1"
-};
+    this.data = {
+      "title": this.employeeForm.value.title,
+      "firstName": this.employeeForm.value.firstName,
+      "lastName": this.employeeForm.value.lastName,
+      "email": this.employeeForm.value.email,
+      "phoneNo": this.employeeForm.value.phone.number,
+      "contact": this.employeeForm.value.phone.number,
+      "dob": this.employeeForm.value.dob,
+      "passport": this.employeeForm.value.passport,
+      "pan": this.employeeForm.value.pan,
+      "adhar": this.employeeForm.value.adhar,
+      "drivingLicence": this.employeeForm.value.drivingLicence,
+      "voterId": this.employeeForm.value.voterId,
+      "status": "1"
+    };
 
-this.EmployeeService.AddEmployee(JSON.stringify(this.data)).subscribe(res => {
-  this.spinner = false;
-  this.toastr.success('Successfully added Employee !!!');
+    this.EmployeeService.AddEmployee(JSON.stringify(this.data)).subscribe(res => {
+      this.spinner = false;
+      this.toastr.success('Successfully added Employee !!!');
 
-})
-this.employeeForm.reset();
-this.spinner = false;
-this.display = false;
+    })
+    this.employeeForm.reset();
+    this.spinner = false;
+    this.display = false;
+
+    this.getAllEmployeesList();
 
 
 
-}
+  }
   markFormTouched(employeeForm: any) {
     throw new Error("Method not implemented.");
   }
-PersonalInfo(){
-this.display = true;
-this.displaylist = false;
-}
-removeSkill(){
-  this.display = false;
-this.displaylist = true;
-}
-addDocument() {
-  this.docArray.push(this.employeeForm.get('ID').value + this.employeeForm.get('idno').value)
-  this.documentArray.push(this.employeeForm.get('idno').value)
-  this.docidArray.push(this.employeeForm.get('ID').value)
-}
-removeDoc(i: any) {
-  this.docArray.splice(i, 1);
+  PersonalInfo() {
+    this.display = true;
+    this.displaylist = false;
+  }
+  removeSkill() {
+    this.display = false;
+    this.displaylist = true;
+  }
+  addDocument() {
+    this.docArray.push(this.employeeForm.get('ID').value + this.employeeForm.get('idno').value)
+    this.documentArray.push(this.employeeForm.get('idno').value)
+    this.docidArray.push(this.employeeForm.get('ID').value);
+
+    if (this.employeeForm.value.ID === 'Adhar Card') {
+      this.employeeForm.value.adhar = this.employeeForm.get('idno').value;
+    } else if (this.employeeForm.value.ID === 'Passport') {
+      this.employeeForm.value.passport = this.employeeForm.get('idno').value;
+    } else if (this.employeeForm.value.ID === 'PAN Card') {
+      this.employeeForm.value.pan = this.employeeForm.get('idno').value;
+    } else if (this.employeeForm.value.ID === 'Driving Lincese') {
+      this.employeeForm.value.drivingLicence = this.employeeForm.get('idno').value;
+    } else if (this.employeeForm.value.ID === 'Voter ID') {
+      this.employeeForm.value.voterId = this.employeeForm.get('idno').value;
+    }
+
+  }
+  removeDoc(i: any) {
+    this.docArray.splice(i, 1);
+  }
+
 }
 
+
+interface DesignationAndDepartment {
+  department: string[];
 }
